99dc83578babcd30c9ce12e0b7e68d5a5ee43d5e
Kevin Chuang <kcxinkun@gmail.com>
2016-06-28 14:06:03 -0700

    notes

--- week_8/day_32_services/inclass/app/js/services/index.js
+++ week_8/day_32_services/inclass/app/js/services/index.js
@@ -1,3 +1,3 @@
 module.exports = function(app) {
-  require('./first_service')(app);
+  require('./first_service')(app); // calling the service immediately since requiring the service immediately returns a function
 };
